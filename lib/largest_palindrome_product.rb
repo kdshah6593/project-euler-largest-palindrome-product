170601656acfc27440e4f5d01930b948ea942c33
Kunal <kdshah6593@gmail.com>
2021-08-16 11:36:56 -0400

    completed solution, all tests passing

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+    numOfDigits = 3 #this could be come an argument for the answer so the class&method can be used for an digit numbers
+    palindromes = []
+    first = second = reset = 1 * 10**(numOfDigits - 1)
+    final = first * 10
+  
+    while first < final
+      while second < final
+        product = (first * second).to_s
+        if product == product.reverse
+          palindromes.push(product.to_i)
+        end
+        second += 1
+      end
+      first += 1
+      second = first
+    end
+  
+    palindromes.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    
+    @@palindromes = []
+
+    def initialize
+    end
+
+    def answer
+        numOfDigits = 3 #this could be come an argument for the answer so the class&method can be used for an digit numbers
+        first = second = reset = 1 * 10**(numOfDigits - 1)
+        final = first * 10
+        
+        while first < final
+            while second < final
+            product = (first * second).to_s
+            if product == product.reverse
+                @@palindromes.push(product.to_i)
+            end
+            second += 1
+            end
+            first += 1
+            second = first
+        end
+        
+        @@palindromes.max
+    end
+end
